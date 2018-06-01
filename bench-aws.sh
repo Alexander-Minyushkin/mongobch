368e36704a09ee277eed394b9756e63b6bb1478a
Alexander Minyushkin <alexander.minushkin@gmail.com>
2018-06-01 17:32:55 +0300

    Script updated to run 4 series of tests

--- bench-aws.sh
+++ bench-aws.sh
@@ -11,7 +11,7 @@ con=$1
 #ping $db_host 
 
 post_num=100000
-log_file=logs/aws_atlas_bench_$post_num.log
+log_file=logs/aws_atlas_bench_June_01.log
 
 ./mongobch.py --connection $con --threads 5 --utt 0.01 --initial_post_num $post_num --label aws-$post_num-5 | tee $log_file
 ./mongobch.py --connection $con --threads 10 --utt 0.01 --label aws-$post_num-10 | tee -a $log_file
@@ -20,19 +20,23 @@ log_file=logs/aws_atlas_bench_$post_num.log
 
 
 post_num=200000
-log_file=logs/aws_atlas_bench_$post_num.log
 
-./mongobch.py --connection $con --threads 5 --utt 0.01 --initial_post_num $post_num --label aws-$post_num-5 | tee $log_file
+./mongobch.py --connection $con --threads 5 --utt 0.01 --initial_post_num $post_num --label aws-$post_num-5 | tee -a $log_file
 ./mongobch.py --connection $con --threads 10 --utt 0.01 --label aws-$post_num-10 | tee -a $log_file
 ./mongobch.py --connection $con --threads 20 --utt 0.01 --label aws-$post_num-20 | tee -a $log_file
 ./mongobch.py --connection $con --threads 40 --utt 0.01 --label aws-$post_num-40 | tee -a $log_file
 
 
 post_num=300000
-log_file=logs/aws_atlas_bench_$post_num.log
 
-./mongobch.py --connection $con --threads 5 --utt 0.01 --initial_post_num $post_num --label aws-$post_num-5 | tee $log_file
+./mongobch.py --connection $con --threads 5 --utt 0.01 --initial_post_num $post_num --label aws-$post_num-5 | tee -a $log_file
 ./mongobch.py --connection $con --threads 10 --utt 0.01 --label aws-$post_num-10 | tee -a $log_file
 ./mongobch.py --connection $con --threads 20 --utt 0.01 --label aws-$post_num-20 | tee -a $log_file
 ./mongobch.py --connection $con --threads 40 --utt 0.01 --label aws-$post_num-40 | tee -a $log_file
 
+post_num=400000
+
+./mongobch.py --connection $con --threads 5 --utt 0.01 --initial_post_num $post_num --label aws-$post_num-5 | tee -a $log_file
+./mongobch.py --connection $con --threads 10 --utt 0.01 --label aws-$post_num-10 | tee -a $log_file
+./mongobch.py --connection $con --threads 20 --utt 0.01 --label aws-$post_num-20 | tee -a $log_file
+./mongobch.py --connection $con --threads 40 --utt 0.01 --label aws-$post_num-40 | tee -a $log_file
