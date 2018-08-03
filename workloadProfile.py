3d166760671d697baf50b5657e57a3a5d631e850
Alexander Minyushkin <alexander.minushkin@gmail.com>
2018-08-03 12:50:21 +0300

    Added ability to repeat workload scenario. Test run on Windows local.

--- workloadProfile.py
+++ workloadProfile.py
@@ -51,7 +51,7 @@ class UserWorkload(threading.Thread):
     Most operations a reads, fewer upvotes, even more fewer comments, least ones are posts.
     """
 
-    def __init__(self, name, sn, user_think_time_sec=10, label="Any-text-to-distinguish-test-runs"):
+    def __init__(self, name, sn, user_think_time_sec=10, label="Any-text-to-distinguish-test-runs", repeat =1):
         threading.Thread.__init__(self, name=name)
 
         self.user_think_time_sec = user_think_time_sec
@@ -64,41 +64,44 @@ class UserWorkload(threading.Thread):
 
         self.label = label
 
+        self.repeat = repeat
+
     def run(self):
 
         timestamp = str(datetime.now())
         print(f'{timestamp}, 0.0, started, 0, {self.getName()}, {self.label}\n')
         
         thread_start = time.time()
-        for action in self.clicks_order:
-            
-            rt = randomText()
-            timestamp = str(datetime.now())
-            info = 0
-            elapsed = 0
+        for _ in range(self.repeat):
+            for action in self.clicks_order:
+                
+                rt = randomText()
+                timestamp = str(datetime.now())
+                info = 0
+                elapsed = 0
 
-            start = time.time()            
-            if action == "read" : 
-                doc = self.sn.read()
-                elapsed = time.time() - start
-                if doc is not None:
-                    info = len(bson.BSON.encode(doc))                    
-            elif action == "upvote" : 
-                start = time.time()
-                self.sn.upvote()
-                elapsed = time.time() - start
-            elif action == "comment" : 
-                start = time.time()
-                self.sn.comment(text = rt)
-                elapsed = time.time() - start
-            elif action == "post" : 
-                start = time.time()
-                self.sn.post(text = rt)  
-                elapsed = time.time() - start                    
+                start = time.time()            
+                if action == "read" : 
+                    doc = self.sn.read()
+                    elapsed = time.time() - start
+                    if doc is not None:
+                        info = len(bson.BSON.encode(doc))                    
+                elif action == "upvote" : 
+                    start = time.time()
+                    self.sn.upvote()
+                    elapsed = time.time() - start
+                elif action == "comment" : 
+                    start = time.time()
+                    self.sn.comment(text = rt)
+                    elapsed = time.time() - start
+                elif action == "post" : 
+                    start = time.time()
+                    self.sn.post(text = rt)  
+                    elapsed = time.time() - start                    
 
-            print(f'{timestamp}, {elapsed}, {action}, {info}, {self.getName()}, {self.label}\n') ## \n added at the end to ensure thread-safe prints
+                print(f'{timestamp}, {elapsed}, {action}, {info}, {self.getName()}, {self.label}\n') ## \n added at the end to ensure thread-safe prints
 
-            time.sleep(self.user_think_time_sec)
+                time.sleep(self.user_think_time_sec)
         
         timestamp = str(datetime.now())
         elapsed = time.time() - thread_start
@@ -116,6 +119,17 @@ class Test_UserWorkload(unittest.TestCase):
         self.assertEqual(sn_dummy.comment_num, 10)
         self.assertEqual(sn_dummy.upvote_num , 50)
         self.assertEqual(sn_dummy.read_num   , 100)
+
+    def test_UserWorkload_with_Repeat(self):
+        sn_dummy = SN_Dummy()
+        mythread = UserWorkload(name = "Test_UserWorkload Thread", sn = sn_dummy, user_think_time_sec=0.01, repeat = 2) 
+        mythread.start()           
+        mythread.join()
+
+        self.assertEqual(sn_dummy.post_num   , 1 * 2)
+        self.assertEqual(sn_dummy.comment_num, 10 * 2)
+        self.assertEqual(sn_dummy.upvote_num , 50 * 2)
+        self.assertEqual(sn_dummy.read_num   , 100 * 2)        
 
         
 
